4433f0840a8745ed374082a36dac9b86d18efc3f
mikecasey12 <ikebudemichael@gmail.com>
2021-06-17 12:22:36 -0700

    script printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * print_hello - Prints "with proper grammar, but the outcome is a piece of art,"
+ */
+
+int main (void)
+{	
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
