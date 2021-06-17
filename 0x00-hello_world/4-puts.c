a6e9f313faf09f056dd4d9a46c63a7d415f04f7c
mikecasey12 <ikebudemichael@gmail.com>
2021-06-17 11:47:51 -0700

    script hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+
 /**
  * function_name - print a message
- * Description: The function displays a message to the users)
+ * Description: The function displays a message to the users
  * Return: void
  */
+
 int main(void)
 {
 	char MyStr[] = "\"Programming is like building a multilingual puzzle";
