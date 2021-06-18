0d0c22450fd2855922c6a3bd23078457f59b9256
mikecasey12 <ikebudemichael@gmail.com>
2021-06-18 08:01:57 -0700

    script print alpabets w/o q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+* main - prints with q and e
+* Return: prints the alphabets in lowercase without q and e
+*/
+
+int main(void)
+{
+	char ch;
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
