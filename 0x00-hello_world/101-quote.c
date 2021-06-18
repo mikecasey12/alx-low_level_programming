ee8d8091175a8b4dcd2bec7e348fde272b3cd4c1
mikecasey12 <ikebudemichael@gmail.com>
2021-06-18 02:26:44 -0700

    script quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char std[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fprintf(stderr, "%s\n", std);
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
