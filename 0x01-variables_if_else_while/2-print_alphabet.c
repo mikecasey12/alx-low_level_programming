148149f19b78d8b8b7f97d76353d132d0e8eb1a0
mikecasey12 <ikebudemichael@gmail.com>
2021-06-18 07:17:19 -0700

    script lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main - prints alphabets
+* Return: prints lowercase alphabets from a-z
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
