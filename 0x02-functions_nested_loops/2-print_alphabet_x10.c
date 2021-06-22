7cb10ac6c3eb0346207b97ca50dc66d1faa7fcff
mikecasey12 <ikebudemichael@gmail.com>
2021-06-22 04:51:17 -0700

    script 10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 10)
+	for (i = 1; i <= 10; i++)
 	{
 		char ch = 'a';
 
-		for (ch = 'a'; ch <= 'z'; ch++)
+		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
 		}
-		i++;
 		_putchar('\n');
 	}
 }
