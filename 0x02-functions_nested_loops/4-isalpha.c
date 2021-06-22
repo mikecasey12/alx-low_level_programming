3efeb504d38e880ac18aa3701850feeb36cb56ba
mikecasey12 <ikebudemichael@gmail.com>
2021-06-22 06:48:25 -0700

    script isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isalpha - a function that prints identifies lowercases and uppercases
+ * @c: single letter input
+ * Return: On success 1.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (0);
+	else
+		return (1);
+}
