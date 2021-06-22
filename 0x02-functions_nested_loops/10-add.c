9fad925b42f797ea8c9b6cf8e5251711793e49a5
mikecasey12 <ikebudemichael@gmail.com>
2021-06-22 11:08:01 -0700

    script add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @n: first number input
+ * @m: second number input
+ * Return: n + m
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
