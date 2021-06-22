56e7b7f622e1cd7a30fef50bc0488f5c6a244f51
mikecasey12 <ikebudemichael@gmail.com>
2021-06-22 10:58:37 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * times_table - a function that prints the multiplication of 9
+ * Return: multiplication of 9.
+ */
+void times_table(void)
+{
+	int r, c, a;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
+		{
+			a = (r * c);
+			if ((a / 10) > 0)
+			{
+				_putchar((a / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((a % 10) + '0');
+
+			if (a < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
