cd8ec329849017b733ebe3c6dc926468aafcb781
mikecasey12 <ikebudemichael@gmail.com>
2021-06-22 10:19:20 -0700

    script jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,34 +5,18 @@
  */
 void jack_bauer(void)
 {
-	for (int t1 = 48; t1 < 51; t1++)
+	int h, m;
+
+	for (h = 0; h < 24; h++)
 	{
-		for (int t2 = 48; t2 <= 57; t2++)
+		for (m = 0; m < 60; m++)
 		{
-			for (int t3 = 48; t3 <= 53; t3++)
-			{
-				for (int t4 = 48; t4 < 58; t4++)
-				{
-					if (!(t1 == 50 && t2 == 51 && t3 == 53 && t4 == 57))
-					{
-						_putchar(t1);
-						_putchar(t2);
-						_putchar(':');
-						_putchar(t3);
-						_putchar(t4);
-						_putchar('\n');
-					} else
-					{
-						_putchar(50);
-						_putchar(51);
-						_putchar(':');
-						_putchar(53);
-						_putchar(57);
-						_putchar('\n');
-						pause();
-					}
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
