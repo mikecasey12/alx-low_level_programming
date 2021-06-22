902c86e5a7810cd20c3fc1158058273f91a30eb9
mikecasey12 <ikebudemichael@gmail.com>
2021-06-22 04:04:03 -0700

    script alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
-
 /**
  * print_alphabet - a function that prints the alphabet 'a-z'
  * Return: On success 0.
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
-	    _putchar(ch);
-	    ch++;
+		_putchar(ch);
+		ch++;
 	}
 	_putchar('\n');
-	return;
 }
