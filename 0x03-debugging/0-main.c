cd95579711aadfcd48a7a7e6fff8883eeb29b26c
mikecasey12 <ikebudemichael@gmail.com>
2021-06-23 18:08:37 -0700

    script multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,23 +3,22 @@
 #include <time.h>
 /**
  * positive_or_negative - printing positive or negative
- * @n: integer input
+ * @i: integer input
  * Return: returns 0 success
  */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	int n;
+	int i;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	printf("%d is positive\n", n);
-	else if (n == 0)
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+	printf("%d is positive\n", i);
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	} else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
-	return (0);
 }
