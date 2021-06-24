3080f2c9611519318eea4fe61285c09efdd84206
mikecasey12 <ikebudemichael@gmail.com>
2021-06-23 18:01:42 -0700

    script multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,25 @@
 #include "holberton.h"
+#include <stdlib.h>
+#include <time.h>
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-int main(void)
+ * positive_or_negative - printing positive or negative
+ * @n: integer input
+ * Return: returns 0 success
+ */
+void positive_or_negative(int n)
 {
-	int i;
+	int n;
 
-	i = 98;
-	positive_or_negative(i);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	printf("%d is positive\n", n);
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
