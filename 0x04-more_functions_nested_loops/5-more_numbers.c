bf44844cd259ab52b61527c38cbfadf483df1cdf
mikecasey12 <ikebudemichael@gmail.com>
2021-06-24 09:29:49 -0700

    script more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,7 +6,7 @@
  * Return: returns 0 on success.
  */
 
-void more_numbers(void);
+void more_numbers(void)
 {
 	int n, m;
 
@@ -18,5 +18,4 @@ void more_numbers(void);
 		}
 		_putchar('\n');
 	}
-	return (0);
 }
