e11570ed1984160153a91b773e780c954655dda3
mikecasey12 <ikebudemichael@gmail.com>
2021-06-24 09:25:00 -0700

    script more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times the number from 0 to 14
+ *
+ * Return: returns 0 on success.
+ */
+
+void more_numbers(void);
+{
+	int n, m;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			_putchar(m + '0');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
