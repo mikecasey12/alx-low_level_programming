766eee1ccb68c3d84efb0f0a808629d99c946aa2
mikecasey12 <ikebudemichael@gmail.com>
2021-06-24 10:59:03 -0700

    script print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of time the character _ should be printed
+ * Return: returns the number of _ that was printed
+ */
+
+void print_line(int n)
+{
+	int m = 0;
+
+	while (m < n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		} else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
