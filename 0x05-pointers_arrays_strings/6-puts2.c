e0c157372b1ae2e47a5eab6b91f6ff834752cb8f
mikecasey12 <ikebudemichael@gmail.com>
2021-06-28 09:34:37 -0700

    script other characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: string input
+ * Return: returns every other character of a string
+ */
+
+void puts2(char *str)
+{
+	int len;
+	char c = s[0];
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		_putchar(s[i++]);
+	}
+	_putchar('\n');
+}
