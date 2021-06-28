225cc429112ed6dbf6e5c87cc1ea002846296847
mikecasey12 <ikebudemichael@gmail.com>
2021-06-28 07:19:03 -0700

    script swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio>
+#include "holberton.h"
+
+/**
+ * swap_int - swaps two integer pointers with a new value
+ * @a: first integer input
+ * @b: second integer input
+ * Return: the swapped value of a as 42 and b as 98
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
