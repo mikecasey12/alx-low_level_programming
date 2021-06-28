958bb7dd10ec5a8a7e760f6bedaa29a4de4625cc
mikecasey12 <ikebudemichael@gmail.com>
2021-06-28 08:59:46 -0700

    script reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_rev - a function that prints a string in reverse
+ * recount is to count to the end, len2 is to count back
+ * @s: string input
+ * Return: returns the reversed of the string
+ */
+
+void print_rev(char *s)
+{
+	int recount = 0;
+	int len, len2;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		recount++;
+	}
+
+	for (len2 = (recount - 1); len2 >=0; len2--)
+	{
+		_putchar(s[len2]);
+	}
+	_putchar('\n');
+}
