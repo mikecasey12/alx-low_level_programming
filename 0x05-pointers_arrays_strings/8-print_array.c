f84c37ae0c5741845725a93c0183d58f9bffc7bc
mikecasey12 <ikebudemichael@gmail.com>
2021-07-02 13:46:36 -0700

    script array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,18 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n - 1; i++)
+	while (i < n)
 	{
-		printf("%d, ", a[i]);
-		if (i == (n - 1))
+		if (i != n - 1)
 		{
-			printf("%d", a[n - 1]);
+			printf("%d", *(a + i));
+			printf(", ");
+		} else
+		{
+			printf("%d\n", *(a + i));
 		}
+		i++;
 	}
-	printf("\n");
 }
